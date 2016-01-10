9a176facb69acc4c9b4b0c9982f022ddae0b65ea
Melanie Burger <melanie.brgr@gmail.com>
2016-01-10 16:47:46 -0500

    quiz app logic complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,111 @@
+"strict mode";
 
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+var ranNum, numberOfGuesses = 0;
+
+function handleOverlay() {
+	// Display modal
+  	$(".what").click( function(){
     	$(".overlay").fadeIn(1000);
-
   	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
+  	// Hide modal
+  	$("a.close").click( function(){
   		$(".overlay").fadeOut(1000);
   	});
+}
 
+function getFeedback( ranNum, userNum ) {
+	// take the generated number and number guessed by the user
+	// if user > 51 away, return "ice cold"
+	// if between 31 and 50, cold
+	// if between 21 and 30, warm
+	// if between 11 and 20, hot
+	// if between 1 and 10, very hot
+	var absDiff = Math.abs( userNum - ranNum );
+	if ( userNum === ranNum ) {
+		return "Yup, you got it. Big deal. Start a new game – unless you're afraid.";
+	}
+	else if ( absDiff <= 10 ) {
+		return "very hot";
+	} else if ( absDiff <= 20 ) {
+		return "hot";
+	} else if ( absDiff <= 30 ) {
+		return "warm";
+	} else if ( absDiff <= 50  ) {
+		return "cold";
+	} else {
+		return "ice cold";
+	}
+}
+
+function displayFeedback( feedback ) {
+	// set feedback to appear in div#feedback
+	$('h2#feedback').text( feedback );
+}
+
+function displayNumberOfGuesses( guesses ) {
+	// set number of guesses in span#count
+	$('span#count').text( guesses );
+}
+
+function displayGuess( guessNum ) {
+	// add each guessed number as an <li> to ul#guessList
+	$('ul#guessList').append('<li>' + guessNum + '</li>');
+}
+
+function incrementGuess() {
+	numberOfGuesses++;
+}
+
+function handleGuess() {
+	// ensure that users provide valid inputs
+	// ensure that the user has supplied a numeric input between 1 and 100
+	$('form').submit( function(e) {
+		e.preventDefault();
+			
+		var guessNum = parseInt( $(this).find('input#userGuess').val() );
+		if ( guessNum%1 !== 0 && guessNum > 100 && guessNum < 0 ) { 
+			alert("Enter a number between 1 and 100, idiot.");
+		} else {
+			// check how hot or cold it is, and display
+			displayFeedback( getFeedback(ranNum, guessNum) );
+			// update number of guesses
+			incrementGuess();
+			displayNumberOfGuesses( numberOfGuesses );
+			// update guess list
+			displayGuess( guessNum );
+			//clear guess
+			$(this).find('input#userGuess').val('');
+		}
+	});
+}
+
+function generateRandomNumber() {
+	//return a random number between 1 and 100
+	return Math.floor(Math.random() * 101);
+}
+
+function newGame() {
+	//do everything necessary to start a new game
+	// generate a new guess number
+	// clear number of guesses
+	// clear guess list
+	ranNum = generateRandomNumber();
+	console.log("The number is " + ranNum + ", cheater.");
+	$('h2#feedback').text( 'Make your Guess!' );
+	$('span#count').text( '0' );
+	$('ul#guessList').empty();
+}
+
+function handleStartNewGame() {
+	//attach handler on button click, start a new game
+	$('li .new').click( newGame );
+}
+
+$(document).ready(function(){
+	newGame();
+	handleStartNewGame();
+	handleGuess();
+	handleOverlay();
 });
 
 
